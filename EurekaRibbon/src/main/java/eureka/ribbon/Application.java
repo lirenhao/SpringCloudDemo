784f8c0c761ff62a9e4ab9d89c28fe99e5b6a4aa
lirenhao <renhao.li@bjyada.com>
2017-10-15 16:57:42 +0800

    init

--- EurekaRibbon/src/main/java/eureka/ribbon/Application.java
+++ EurekaRibbon/src/main/java/eureka/ribbon/Application.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class Application {
 
     @Bean //定义REST客户端，RestTemplate实例
-    @LoadBalanced //开启负债均衡的能力
+    @LoadBalanced //开启负载均衡的能力
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
