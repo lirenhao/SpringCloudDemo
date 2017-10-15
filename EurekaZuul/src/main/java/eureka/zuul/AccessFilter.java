10b7f5d547644ca7bfff470b7e046443a6cf8416
lirenhao <renhao.li@bjyada.com>
2017-10-15 21:59:45 +0800

    路由转发

---
+++ EurekaZuul/src/main/java/eureka/zuul/AccessFilter.java
@@ -0,0 +1,31 @@
+package eureka.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    public Object run() {
+        return null;
+    }
+}
---
+++ EurekaZuul/src/main/java/eureka/zuul/Application.java
@@ -0,0 +1,23 @@
+package eureka.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
+}
