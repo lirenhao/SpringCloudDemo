4ebe6b410ce4de5c278c750c26b500828a51b409
lirenhao <renhao.li@bjyada.com>
2017-10-15 16:55:41 +0800

    init

---
+++ EurekaCompute/src/main/java/eureka/compute/ServiceInstanceRestController.java
@@ -0,0 +1,24 @@
+package eureka.compute;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient; //服务发现客户端
+
+    @GetMapping(value = "/add")
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+        Integer r = a + b;
+        System.out.println("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
+    }
+}
+
---
+++ EurekaRibbon/src/main/java/eureka/ribbon/ConsumerController.java
@@ -0,0 +1,18 @@
+package eureka.ribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/add")
+    public String add() {
+        return restTemplate.getForEntity("http://EUREKA-COMPUTE/add?a=10&b=20", String.class).getBody();
+    }
+}
